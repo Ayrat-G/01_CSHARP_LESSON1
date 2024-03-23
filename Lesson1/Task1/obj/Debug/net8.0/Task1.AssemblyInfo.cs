c8d61468d966cd6b86c6027f56471683e7117d70
Ayrat Giniyatullin <AyratGiniyatullin@gmail.com>
2024-03-24 01:30:24 +0500

    Добавили функции деления

--- Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b771ded610fd49e05318952ced564006ccb35df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daf55552308ee4bdb0ae9dfb7a8919043d04f631")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
